cb5a0fe1c971e60a0ac53bda7bad3d7910f55f5e
stxndli <silemsifeddine@gmail.com>
2022-12-27 02:48:52 +0100

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import {RecoilRoot} from 'recoil'
 export default function App({ Component, pageProps }: AppProps) {
   return <RecoilRoot>
     <Component {...pageProps} />
+    <Analytics />
   </RecoilRoot>
 }
